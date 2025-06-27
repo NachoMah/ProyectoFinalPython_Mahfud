b928ff42ad19e4c9dd68fb441f3c7cae980f8870
NachoMah <ignaciomahfud@gmail.com>
2025-06-27 02:42:23 -0300

    Algunos modelos (Cliente, Producto y Empleado) fueron creados, las migraciones fueron aplicadas y la base de datos (SQlite3) fue creada

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)
     
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    informacion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return self.nombre
+    
+    
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=100)
+    puesto = models.CharField(max_length=100)
+    dni = models.CharField(max_length=20, unique=True)
+    
+    def __str__(self):
+        return f"{self.nombre} ({self.puesto})"
