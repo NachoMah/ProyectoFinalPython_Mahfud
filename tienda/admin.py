85ef7ede5c91a1f889990daee43fe07607b08d4c
NachoMah <ignaciomahfud@gmail.com>
2025-07-15 18:55:03 -0300

    Se mejora un poco más la visualizacion de datos del admin para los modelos

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
-from .models import Cliente, Producto, Empleado, Publicacion
+from .models import Cliente, Producto, Empleado, Publicacion, Perfil
 
-admin.site.register(Cliente)
-admin.site.register(Producto)
-admin.site.register(Empleado)
-admin.site.register(Publicacion)
+@admin.register(Publicacion)
+class PublicacionAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'subtitulo', 'fecha')
+    search_fields = ('titulo', 'subtitulo')
+    list_filter = ('fecha',)
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'correo')
+    search_fields = ('nombre', 'correo')
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio')
+    search_fields = ('nombre',)
+    list_filter = ('precio',)
+
+@admin.register(Empleado)
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'puesto', 'dni')
+    search_fields = ('nombre', 'puesto')
+
+@admin.register(Perfil)
+class PerfilAdmin(admin.ModelAdmin):
+    list_display = ('user', 'fecha_nacimiento')
+    search_fields = ('user__username',)
